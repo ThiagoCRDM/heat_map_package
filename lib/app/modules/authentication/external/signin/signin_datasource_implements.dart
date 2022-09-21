2e19cb7bea8e7fe3494626ded0129905d7e2779e
Thiago Cesar <thiagocesarmata@gmail.com>
2022-09-21 01:24:02 -0300

    feat: sigin data source implementation

--- lib/app/modules/authentication/external/signin/signin_datasource_implements.dart
+++ lib/app/modules/authentication/external/signin/signin_datasource_implements.dart
@@ -1,29 +1,25 @@
-import 'package:dio/dio.dart';
 import 'package:heat_map_package/app/modules/authentication/domain/errors/errors.dart';
 import 'package:heat_map_package/app/modules/authentication/domain/models/signin_models.dart';
 import 'package:heat_map_package/app/modules/authentication/infra/datasources/signin_datasource.dart';
 
-class SiginDataSource implements ISigninDataSource {
-  final Dio _dio;
+import '../errors/http_errors.dart';
+import '../http/http_client_interface.dart';
 
-  SiginDataSource(this._dio);
+class SiginDataSource implements ISigninDataSource {
+  final IHttpClient _client;
+
+  SiginDataSource(this._client);
 
   String get _apiURL => "http://localhost:5050/api/signin";
   @override
   Future<String> signin(SigninModel signin) async {
     try {
-      final response = await _dio.post<String>(_apiURL,
+      final response = await _client.post(_apiURL,
           data: {"email": signin.email, "password": signin.password});
 
-      if (response.statusCode != 200) {
-        throw Exception("error unexpected");
-      }
-
-      if (response.data == null) {
-        throw const SigninError(message: "error trying to login");
-      }
-
-      return response.data!;
+      return response!["token"];
+    } on HttpError {
+      rethrow;
     } catch (e) {
       throw const SigninError(message: "error trying to login");
     }
